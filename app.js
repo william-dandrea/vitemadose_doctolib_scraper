9a64ad07f288bda7ceeaa53b8d6e0c52f84c26d1
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2021-05-13 00:56:51 +0200

    Changes by williamdandrea

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const https = require('https');
 const discordClient = new Discord.Client();
 discordClient.login(configDiscord.BOT_TOKEN);
 
+let urlDestination = "https://www.doctolib.fr/centre-de-sante/mougins-mouans-sartoux/centre-de-vaccination-covid-19-mougins-mouans-sartoux?highlight%5Bspeciality_ids%5D%5B%5D=5494"
+let url = "https://www.doctolib.fr/booking/centre-de-vaccination-covid-castellane";
+// let url = "https://www.doctolib.fr/booking/centre-de-vaccination-covid-19-mougins-mouans-sartoux";
+
+
+
 let botCovidChannel = null;
 
 const getBotCovidChannel = async () => {
@@ -17,8 +23,8 @@ function launchedWhenAvailableAppointment(urlIntern) {
     botCovidChannel.send("Nouveau creneau dispo !");
     console.log(urlDestination);
 }
-function launchedWhenUnavailableAppointment(url) {
-    botCovidChannel.send("Pas de creneau dispo")
+function launchedWhenUnavailableAppointment(urlIntern) {
+    //botCovidChannel.send("Pas de creneau dispo a cette url : " + urlDestination)
     console.log("Pas de creneau dispo");
 }
 function determineIfweHaveDisponibilities(urls) {
@@ -45,45 +51,55 @@ function determineIfweHaveDisponibilities(urls) {
         });
     }
 }
-let url = "https://www.doctolib.fr/booking/centre-de-vaccination-covid-19-mougins-mouans-sartoux";
-https.get(url,(res) => {
-    let body = "";
-    res.on("data", (chunk) => {
-        body += chunk;
+
+
+
+async function launch() {
+    await getBotCovidChannel();
+    https.get(url,(res) => {
+        let body = "";
+        res.on("data", (chunk) => {
+            body += chunk;
+        });
+        res.on("end", () => {
+            try {
+                let json = JSON.parse(body);
+                // do something with JSON
+                let practice_id = json.data.profile.id;
+                let visit_motive_ides = json.data.visit_motives.filter(motive => motive.name.includes("1re injection vaccin COVID-19")).map(elem => elem.id);
+                let agendas = json.data.agendas.filter(motivid => motivid.visit_motive_ids.some(r => visit_motive_ides.includes(r))).map(elem => elem.id);
+                console.log(visit_motive_ides);
+                console.log(agendas);
+                let agendasString = agendas.join("-");
+                let curentDate = new Date();
+                let date = curentDate.getFullYear() + '-0' + (curentDate.getMonth()+1) + '-' + (curentDate.getDate() + 1);
+                console.log(date);
+                let urls = [];
+                for (let i = 0; i < visit_motive_ides.length; i++) {
+                    let firstUrl = "https://www.doctolib.fr/availabilities.json?" +
+                        "start_date=" + date + "&" +
+                        "visit_motive_ids=" + visit_motive_ides[i] + "&" +
+                        "agenda_ids=" + agendasString +"&" +
+                        "practice_ids=" + practice_id + "&" +
+                        "telehealth=false&" +
+                        "destroy_temporary=true&" +
+                        "limit=5"
+                    urls.push(firstUrl)
+                }
+                console.log(urls);
+                determineIfweHaveDisponibilities(urls);
+            } catch (error) {
+                console.error(error.message);
+            };
+        });
+    }).on("error", (error) => {
+        console.error(error.message);
     });
-    res.on("end", () => {
-        try {
-            let json = JSON.parse(body);
-            // do something with JSON
-            let practice_id = json.data.profile.id;
-            let visit_motive_ides = json.data.visit_motives.filter(motive => motive.name.includes("1re injection vaccin COVID-19")).map(elem => elem.id);
-            let agendas = json.data.agendas.filter(motivid => motivid.visit_motive_ids.some(r => visit_motive_ides.includes(r))).map(elem => elem.id);
-            console.log(visit_motive_ides);
-            console.log(agendas);
-            let agendasString = agendas.join("-");
-            let curentDate = new Date();
-            let date = curentDate.getFullYear() + '-0' + (curentDate.getMonth()+1) + '-' + (curentDate.getDate() + 1);
-            console.log(date);
-            let urls = [];
-            for (let i = 0; i < visit_motive_ides.length; i++) {
-                let firstUrl = "https://www.doctolib.fr/availabilities.json?" +
-                    "start_date=" + date + "&" +
-                    "visit_motive_ids=" + visit_motive_ides[i] + "&" +
-                    "agenda_ids=" + agendasString +"&" +
-                    "practice_ids=" + practice_id + "&" +
-                    "telehealth=false&" +
-                    "destroy_temporary=true&" +
-                    "limit=5"
-                urls.push(firstUrl)
-            }
-            determineIfweHaveDisponibilities(urls);
-        } catch (error) {
-            console.error(error.message);
-        };
-    });
-}).on("error", (error) => {
-    console.error(error.message);
-});
+}
+
+setInterval(launch,2500);
+
+
 // https://www.doctolib.fr/availabilities.json?
 // start_date=2021-05-12&
 // visit_motive_ids=2532715&
