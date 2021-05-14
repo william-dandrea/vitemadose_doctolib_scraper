dac1824978a12fb479db3d55ef66eb7dcde3aac6
DANDREA WILLIAM <william.d'andrea@etu.unice.fr>
2021-05-14 13:32:55 +0200

    Afficher uniquement 1 creneau toutes les 15 minute si tout le temps libre / Amelioration du filtrage pour prendre en cmpte la nouvelle version de l'API doctolib

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ discordClient.login(configDiscord.BOT_TOKEN);
 // let url = "https://www.doctolib.fr/booking/centre-de-vaccination-covid-19-mougins-mouans-sartoux";
 
 
-
+let urlsTime = [];
 let botCovidChannel = null;
 
 const getBotCovidChannel = async () => {
@@ -20,12 +20,42 @@ const getBotCovidChannel = async () => {
 }
 //const botCovidChannel=await getBotCovidChannel();
 function launchedWhenAvailableAppointment(urlIntern, urlDestination) {
-    botCovidChannel.send("Nouveau creneau disponible ! à : " + urlDestination);
-    console.log(urlDestination);
+
+
+    if (urlsTime.filter(elem => elem[0] === urlDestination).length === 0) {
+        urlsTime.push([urlDestination, Date.now()]);
+        botCovidChannel.send("Nouveau creneau disponible ! à : " + urlDestination);
+    } else {
+
+        const currentLink = urlsTime.find(element => element[0] === urlDestination);
+
+
+        if (Date.now() - currentLink[1] >= 900000) {
+            currentLink[0] = Date.now();
+            botCovidChannel.send("Nouveau creneau disponible ! à : " + urlDestination);
+        }
+
+
+    }
+
+
+
+    // botCovidChannel.send("Nouveau creneau disponible ! à : " + urlDestination);
+    // console.log(urlDestination);
+    // console.log(urlsTime);
 }
 function launchedWhenUnavailableAppointment(urlIntern, urlDestination) {
-    //botCovidChannel.send("Pas de creneau dispo a cette url : " + urlDestination)
-    console.log("Pas de creneau dispo");
+
+    console.log("unavailable");
+    if (urlsTime.filter(elem => elem[0] === urlDestination).length === 0) {
+        const currentLink = urlsTime.find(element => element[0] === urlDestination);
+
+        const index = urlsTime.indexOf(currentLink);
+        if (index > -1) {
+            urlsTime.splice(index, 1);
+        }
+    }
+
 }
 function determineIfweHaveDisponibilities(urls, urlDestination) {
     for (let i = 0; i < urls.length; i++) {
@@ -37,7 +67,7 @@ function determineIfweHaveDisponibilities(urls, urlDestination) {
             res.on("end", () => {
                 try {
                     let json = JSON.parse(body);
-                    if (json.availabilities.length !== 0) {
+                    if (json.availabilities.length !== 0 && json.availabilities[0].slots.length !== 0) {
 
                         launchedWhenAvailableAppointment(urls[i], urlDestination);
                     } else {
@@ -67,14 +97,20 @@ async function launch(url, urlDestination) {
                 let json = JSON.parse(body);
                 // do something with JSON
                 let practice_id = json.data.profile.id;
+
+
                 let visit_motive_ides = json.data.visit_motives.filter(motive => motive.name.includes("1re injection vaccin COVID-19")).map(elem => elem.id);
+
+                if (json.data.visit_motive_categories.find(motive => motive.name.includes("Grand public"))) {
+                    let visit_motive_categories_intern = json.data.visit_motive_categories.find(motive => motive.name.includes("Grand public")).id;
+                    visit_motive_ides = json.data.visit_motives.filter(motive => motive.name.includes("1re injection vaccin COVID-19") && visit_motive_categories_intern === motive.visit_motive_category_id).map(elem => elem.id);
+                }
+
                 let agendas = json.data.agendas.filter(motivid => motivid.visit_motive_ids.some(r => visit_motive_ides.includes(r))).map(elem => elem.id);
-                console.log(visit_motive_ides);
-                console.log(agendas);
                 let agendasString = agendas.join("-");
                 let curentDate = new Date();
                 let date = curentDate.getFullYear() + '-0' + (curentDate.getMonth()+1) + '-' + (curentDate.getDate() + 1);
-                console.log(date);
+                // console.log(date);
                 let urls = [];
                 for (let i = 0; i < visit_motive_ides.length; i++) {
                     let firstUrl = "https://www.doctolib.fr/availabilities.json?" +
@@ -87,7 +123,6 @@ async function launch(url, urlDestination) {
                         "limit=5"
                     urls.push(firstUrl)
                 }
-                console.log(urls);
                 determineIfweHaveDisponibilities(urls, urlDestination);
             } catch (error) {
                 console.error(error.message);
@@ -103,8 +138,11 @@ if (process.env.NODE_APP_INSTANCE === '0' || process.env.NODE_APP_INSTANCE === u
 
         centreVaccinationNice.elements.forEach(centre => {
             launch(centre.url_api, centre.url_destination);
-        })
+        });
+        console.log(urlsTime.length);
     },2500);
+
+
 }
 
 
